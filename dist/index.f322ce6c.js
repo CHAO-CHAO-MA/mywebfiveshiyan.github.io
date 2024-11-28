f461c6fd5e792f286eba9d4995083e4f7efc41ac
CHAO-CHAO-MA <2647201747@qq.com>
2024-11-28 20:21:01 +0800

    提交信息

---
+++ dist/index.f322ce6c.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var serviceButton = document.getElementById('serviceButton');
+    serviceButton.addEventListener('click', function() {
+        alert("\u670D\u52A1\u6B63\u5728\u5F00\u53D1\u4E2D\uFF0C\u656C\u8BF7\u671F\u5F85\uFF01");
+    });
+});
+
+//# sourceMappingURL=index.f322ce6c.js.map
